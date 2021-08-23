ab9fbd4b5481da1fa35b97a2263439e757f7bd25
Seung1110 <iiwvii@naver.com>
2021-08-23 17:44:38 +0900

    #3

--- 이승엽/Algorithm/전화번호목록(이승엽).java
+++ 이승엽/Algorithm/전화번호목록(이승엽).java
@@ -3,14 +3,15 @@ class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         
-        Arrays.sort(phone_book);
+        Arrays.sort(phone_book); // 12 123 124 129 1345 14 
         
         for(int i = 0; i < phone_book.length; i++){
             for(int j = i+1; j < phone_book.length; j++){
                 if(phone_book[j].startsWith(phone_book[i])){
                     return false;
                 }
-                if(phone_book[i].length() > phone_book[j].length() || phone_book[i].compareTo(phone_book[j].substring(0,phone_book[i].length())) != 1 ){
+                if(phone_book[i].length() > phone_book[j].length() 
+                		|| phone_book[i].compareTo(phone_book[j].substring(0,phone_book[i].length())) != 1 ){
                     break;
                 }
 
