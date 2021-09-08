f819b2b3adc1e7571be8439aac7ac5606f4975a6
EasyBAMM <huhjb1020@naver.com>
2021-09-08 20:55:58 +0900

    #30

---
+++ 허준범/SQL/입양시각구하기(2)(허준범).sql
@@ -0,0 +1,13 @@
+-- 코드를 입력하세요
+-- 답 봄
+SELECT a.HOUR, NVL(b.COUNT, 0) AS "COUNT" 
+FROM (
+    SELECT LEVEL-1 AS "HOUR"
+    FROM dual CONNECT BY LEVEL < 25
+) a, (
+    SELECT TO_CHAR(datetime, 'HH24') AS "HOUR", COUNT(*) AS "COUNT" 
+    FROM animal_outs 
+    GROUP BY TO_CHAR(datetime, 'HH24')
+) b
+WHERE a.HOUR = b.HOUR(+)
+ORDER BY a.HOUR;
