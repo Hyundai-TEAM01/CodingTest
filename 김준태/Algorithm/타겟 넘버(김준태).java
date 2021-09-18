a78fc2e875a742bf374af9600a4f563010babd4a
JunTae Kim <emflaxl100@naver.com>
2021-09-18 23:47:09 +0900

    #43

---
+++ 김준태/Algorithm/타겟 넘버(김준태).java
@@ -0,0 +1,31 @@
+class Solution {
+    static int count = 0;
+    
+    public static void dfs(int[] numbers, int depth, int target, int value){
+        
+        // 종료 조건
+        if (depth  == numbers.length - 1){
+            if (value == target){
+                System.out.println(depth + " 일 때 " + value);
+                count++;
+                return;
+            }
+        }
+        
+        if ( depth < numbers.length ){
+            System.out.println(depth + 1 + " " + numbers[depth] + " value : " + value);
+            dfs(numbers, depth + 1, target, value + numbers[depth]);
+            dfs(numbers, depth + 1, target, value - numbers[depth]);
+        }
+    }
+    
+    
+    
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        
+        dfs(numbers, 0, target, 0);
+        
+        return answer = count;
+    }
+}
