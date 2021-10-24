b9eea0554153ab0192fbd6cf38b5705339a1e782
EasyBAMM <huhjb1020@naver.com>
2021-10-24 18:24:20 +0900

    #62

---
+++ 허준범/SQL/DrawTheTriangle1(허준범).sql
@@ -0,0 +1,17 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SET SERVEROUTPUT ON;
+DECLARE
+I NUMBER;
+J NUMBER;
+BEGIN
+FOR I IN REVERSE 1..20 LOOP
+    FOR J IN 1..I LOOP
+    DBMS_OUTPUT.PUT('* ');
+    END LOOP;
+    DBMS_OUTPUT.NEW_LINE;
+END LOOP;
+END;
+/
